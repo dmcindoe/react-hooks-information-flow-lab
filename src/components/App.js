754e63a1cbcd59a449c99cd9ec578319a9c2a232
Andrew McIndoe <amcindoe@powerhrg.com>
2021-07-06 14:10:57 -0400

    R-Hooks Info Flow Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,22 @@
-import React, { useState } from "react";
-import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import React, { useState } from "react"
+import ShoppingList from "./ShoppingList"
+import itemData from "../data/items"
+import Header from "./Header.js"
 
 function App() {
   const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false)
 
-  function handleDarkModeClick() {
+  const handleDarkModeClick = () => {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
-
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
       <ShoppingList items={items} />
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
